7f693d892c3eb97da9d83dc5780278d8482dc26b
medabkasm <51716754+medabkasm@users.noreply.github.com>
2019-07-28 02:20:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -58,13 +58,13 @@ def move_to_course_directory(title):
     except:
         print('Could not create subdirectory for the course: {}'.format(title))
 
-
+'''
 def getID(link):
     #Go to the web page with the video and extract its ID
     ideoAttrs = link.find('video').attrs
     videoId = videoAttrs['data-video-id']
     return videoId
-
+'''
 
 def removeReservedChars(value):
     """ Remove reserved characters because of Windows OS compatibility
@@ -185,7 +185,8 @@ for link in open('links.txt'):
                 ydl.download([videolink])
 
                 if (SUBTITLES):
-                    videoId = getID(soup)
+                    ideoAttrs = link.find('video').attrs
+                    videoId = videoAttrs['data-video-id']
                     info = ydl.extract_info(videolink, download=False)
                     name = info.get('title', None)
                     subs = getSubtitles(videoId, name)
