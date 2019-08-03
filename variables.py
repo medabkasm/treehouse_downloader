61a4e68f146f7e92302bb1abc64044b758a264a5
mohamed abkasm <51716754+medabkasm@users.noreply.github.com>
2019-08-03 17:59:22 +0100

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,48 @@
+
+currentValueLinks = 0
+maxValueLinks = 0
+maxValueVideos = 0
+currentValueVideos = 0
+session = None
+SKIP_LOGIN = False
+
+# global variables for the login page .seted by the main function.
+root = None
+myUserName = None
+myPassword = None
+# global variables for the login home page .seted by the main function.
+homePage = None
+linksFileDir = None
+aboveFrame = None
+linksFileButton = None
+linksFile = None
+linksFileEntry = None
+SUBTITLES_check = None
+VIDEOS_check = None
+centerFrame = None
+subtitleCheck = None
+videosChek = None
+filling = None
+linkText = None
+videoText = None
+videoLabel = None
+linkLabel = None
+progressBarLinks = None
+progressBarVideos = None
+downFrame2 = None
+startButton = None
+cancelButton = None
+login = None
+downFrame = None
+progressText = None
+
+
+# Download subtitles of the videos - use 'True' to download subtitles
+SUBTITLES = 0
+VIDEOS = 1
+TEXT = 'Nothing to download .'
+# Download accelerator
+EXTERNAL_DL = 'aria2c'
+
+# Video format - webm or mp4
+VIDEO_FORMAT = 'webm'
